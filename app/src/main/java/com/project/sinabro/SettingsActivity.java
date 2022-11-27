66456265c07b3adbd6f89bb028c213c3bdd72c54
Yoonhee kim <kss7035@naver.com>
2022-11-27 15:08:08 +0900

    [feat]:(설정) 이미지 버튼 생성 후 log test 실행 (#29)

--- app/src/main/java/com/project/sinabro/SettingsActivity.java
+++ app/src/main/java/com/project/sinabro/SettingsActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -25,7 +26,13 @@ public class SettingsActivity extends Fragment {
     private String mParam1;
     private String mParam2;
 
-    private ImageButton back_ibtn;
+    private ImageButton back_ibtn;                      // 뒤로가기 버튼
+    private ImageButton Notifications_imgbtn;           // Notification 버튼
+    private ImageButton Userpolicy_imgbtn;              // use policy 버튼
+    private ImageButton Language_imgbtn;                // Language 버튼
+    private ImageButton Delete_histoty_imgbtn;          // Delete histoty 버튼
+    private ImageButton TextSize_imgbtn;                // TextSize 버튼
+
 
     public SettingsActivity() {
         // Required empty public constructor
@@ -72,6 +79,51 @@ public class SettingsActivity extends Fragment {
                 getActivity().onBackPressed();
             }
         });
+
+        // Notification 페이지로 넘어가기
+        Notifications_imgbtn =(ImageButton) view.findViewById(R.id.Notifications_imgbtn);
+        Notifications_imgbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d("Notification", "onClick: 테스트 중입니다.");
+            }
+        });
+        // use policy 페이지로 넘어가기
+        Userpolicy_imgbtn = (ImageButton) view.findViewById(R.id.Userpolicy_imgbtn);
+        Userpolicy_imgbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d("usepolicy", "onClick: 테스트 중입니다.");
+            }
+        });
+        // 텍스트 크기 설정
+        TextSize_imgbtn = (ImageButton) view.findViewById(R.id.TextSize_imgbtn);
+        TextSize_imgbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d("텍스트 크기변경", "onClick: 테스트 중입니다.");
+            }
+        });
+        //언어 설정
+        Language_imgbtn =(ImageButton) view.findViewById(R.id.Language_imgbtn);
+        Language_imgbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d("언어설정", "onClick: 테스트 중입니다.");
+            }
+        });
+        // 사용기록 삭제
+        Delete_histoty_imgbtn= (ImageButton) view.findViewById(R.id.Delete_histoty_imgbtn);
+        Delete_histoty_imgbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d("사용기록 삭제", "onClick: 테스트 중입니다.");
+            }
+        });
+
+
+
+
 
         return view;
     }
