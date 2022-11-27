2c8a6f64acef6b7e617cef3dd44c9362ecea1086
Yoonhee kim <kss7035@naver.com>
2022-11-27 14:59:36 +0900

    [feat]:(설정) NotificationActivity 생성  (#29)

---
+++ app/src/main/java/com/project/sinabro/NotificationActivity.java
@@ -0,0 +1,71 @@
+package com.project.sinabro;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageButton;
+
+import androidx.fragment.app.Fragment;
+
+public class NotificationActivity extends Fragment {
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    private ImageButton back_ibtn;
+
+    public NotificationActivity() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment SettingsActivity.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static SettingsActivity newInstance(String param1, String param2) {
+        SettingsActivity fragment = new SettingsActivity();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_settings_activity, container, false);
+
+        // 뒤로가기 버튼 기능
+        back_ibtn = (ImageButton) view.findViewById(R.id.back_ibtn);
+        back_ibtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                getActivity().onBackPressed();
+            }
+        });
+
+        return view;
+    }
+}
