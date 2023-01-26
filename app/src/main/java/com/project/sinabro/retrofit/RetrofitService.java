9f28f6ec9a63bae4b44bd105ff5986ae5ec218fa
developersung13 <developersung13@gmail.com>
2023-01-26 23:29:56 +0900

    [feat]: sinabroServer 통신을 위한 retrofit 서비스 파일 추가 (#40)

---
+++ app/src/main/java/com/project/sinabro/retrofit/RetrofitService.java
@@ -0,0 +1,25 @@
+package com.project.sinabro.retrofit;
+
+import com.google.gson.Gson;
+
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class RetrofitService {
+    private Retrofit retrofit;
+
+    public RetrofitService() {
+        initializeRetrofit();
+    }
+
+    private void initializeRetrofit() {
+        retrofit = new Retrofit.Builder()
+                .baseUrl("http://개인의 공인IP:9000")
+                .addConverterFactory(GsonConverterFactory.create(new Gson()))
+                .build();
+    }
+
+    public Retrofit getRetrofit() {
+        return retrofit;
+    }
+}
