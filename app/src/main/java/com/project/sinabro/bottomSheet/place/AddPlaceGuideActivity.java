d9ff9c2d878b7e266a963545af6a61f24d278b07
developersung13 <developersung13@gmail.com>
2023-02-17 13:59:12 +0900

    [feat]: 장소 등록 이전 안내 문구 액티비티 추가 (#25)

---
+++ app/src/main/java/com/project/sinabro/bottomSheet/place/AddPlaceGuideActivity.java
@@ -0,0 +1,46 @@
+package com.project.sinabro.bottomSheet.place;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+import com.project.sinabro.R;
+
+public class AddPlaceGuideActivity extends AppCompatActivity {
+
+    private ImageButton back_iBtn;
+    private Button goAddPlace_btn;
+    private TextView menuTitle_tv;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_place_guide);
+
+        // 뒤로가기 버튼 기능
+        back_iBtn = findViewById(R.id.back_iBtn);
+        back_iBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onBackPressed(); // 뒤로가기 기능 수행
+                finish(); // 현재 액티비티 종료
+            }
+        });
+
+        menuTitle_tv = findViewById(R.id.menuTitle_tv);
+
+        goAddPlace_btn = findViewById(R.id.goAddPlace_btn);
+        goAddPlace_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final Intent intent = new Intent (getApplicationContext(), AddLocationInfoActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
