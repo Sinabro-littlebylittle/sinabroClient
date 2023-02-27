e6a52ffc79360ce36f066316737773726ac62fff
YEOLAM <56868605+developersung13@users.noreply.github.com>
2023-02-28 02:50:49 +0900

    [feat]: Sidebar Navigation 내 "개발 정보" 메뉴 액티비티 설계 (#58) (#59)
    * [feat]: 외부 이미지 로딩을 위해 glide 프레임워크 추가 (#58)
    
    * [feat]: (오픈소스 라이선스) 액티비티 추가에 따른 AndroidManifest 내 액티비티 정의 라인 추가 (#58)
    
    * [feat]: (개발 정보) Fragment 파일명 변경으로 인한 기존 파일 제거 (#58)
    
    * [feat]: 우측 방향 화살표 XML 파일명 수정으로 인한 기존 파일 제거 (#58)
    
    * [feat]: color 파일 내 회색 색상명 변경으로 인한 지정 name 수정 (#58)
    
    * [feat]: '장소 등록' 액티비티 내 메뉴명 주석 올바르게 수정 (#58)
    
    * [feat]: '개발 문의' fragment 제어 자바 파일명 변경으로 인한 기존 파일 제거 (#58)
    
    * [feat]: RoundedImageView 요소 사용 코드 id명 수정 (#58)
    
    * [feat]: color 파일 내 회색 색상명 변경으로 인한 지정 name 수정 (#58)
    
    * [feat]: '개발 정보' fragment 파일명 및 id명 변경에 따른 name/layout 지정값 변경 (#58)
    
    * [feat]: 신규 색상 추가 및 기존 색상명 일부 변경 (#58)
    
    * [feat]: '개발 정보' fragment 전용 string 및 '오픈소스 라이선스' 액티비티 전용 string 데이터 추가 (#58)
    
    * [feat]: 기존 AboutUsFragment 파일 제거 후 DevInfoFragment으로 대체 및 기능 코드 추가 (#58)
    
    * [feat]: '오픈소스 라이선스' 액티비티 자바 파일 추가 (#58)
    
    * [faet]: 오른쪽 방향 푸른 화살표 XML 파일 추가 (#58)
    
    * [faet]: 오른쪽 방향 회색 화살표 XML 파일 추가 (#58)
    
    * [feat]: 목표 관련 이미지 파일 추가 (#58)
    
    * [feat]: 소형 정보 레이아웃 배경 전용 XMl 파일 추가 (#58)
    
    * [feat]: 글자 하단 밑줄 XML 파일 추가 (#58)
    
    * [feat]: '오픈소스 라이선스' 액티비티 XMl 파일 추가 (#58)
    
    * [feat]: '개발 정보' fragment XMl 파일 추가 (#58)
    
    * [feat]: Sidebar Navigation 내 '문의' 메뉴 클릭 시 이벤트 리스너 로직 제거 (#58)
    
    * [feat]: Sidebar Navigation 내 '문의' fragment 연결 로직 수정 (#58)
    
    * [feat]: '개발 정보' fragment 내 '개발목표', '프로젝트 오픈소스' TextView 전용 string 데이터 내 개행 문자 제거 (#58)
    
    * [feat]: '문의' fragment 제어 자바 파일 추가 (#58)
    
    * [feat]: '오픈소스 라이선스' 액티비티 내 MIT 라이선스 관련 요소 크기 조정 (#58)
    
    * [feat]: '개발 정보' fragment 일부 TextView 요소 크기 조정 (#58)

--- app/src/main/java/com/project/sinabro/sideBarMenu/support/SupportFragment.java
+++ app/src/main/java/com/project/sinabro/sideBarMenu/support/SupportFragment.java
@@ -1,5 +1,7 @@
-package com.project.sinabro.sideBarMenu.aboutUs;
+package com.project.sinabro.sideBarMenu.support;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,29 +9,26 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageButton;
 
 import com.project.sinabro.R;
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link AboutUsFragment#newInstance} factory method to
+ * Use the {@link SupportFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class AboutUsFragment extends Fragment {
+public class SupportFragment extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
 
-    private ImageButton back_ibtn;
-
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
 
-    public AboutUsFragment() {
+    public SupportFragment() {
         // Required empty public constructor
     }
 
@@ -39,11 +38,11 @@ public class AboutUsFragment extends Fragment {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment AboutUsActivity.
+     * @return A new instance of fragment SupportFragment.
      */
     // TODO: Rename and change types and number of parameters
-    public static AboutUsFragment newInstance(String param1, String param2) {
-        AboutUsFragment fragment = new AboutUsFragment();
+    public static SupportFragment newInstance(String param1, String param2) {
+        SupportFragment fragment = new SupportFragment();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -64,16 +63,23 @@ public class AboutUsFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View view = inflater.inflate(R.layout.fragment_about_us_activity, container, false);
+        View view = inflater.inflate(R.layout.fragment_empty, container, false);
+        /** 메일 문의 선택 방식(1) */
+        Intent intent = new Intent(Intent.ACTION_SENDTO);
+        intent.setData(Uri.parse("mailto:")); // only email apps should handle this
+        intent.putExtra(Intent.EXTRA_EMAIL, new String[]{"cbnusinabro@gmail.com"});
+        intent.putExtra(Intent.EXTRA_SUBJECT, "[문의]: ");
+        startActivity(intent);
 
-        // 뒤로가기 버튼 기능
-        back_ibtn = (ImageButton) view.findViewById(R.id.back_ibtn);
-        back_ibtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                getActivity().onBackPressed();
-            }
-        });
+        /** 메일 문의 선택 방식(2)  */
+        // Intent selectorIntent = new Intent(Intent.ACTION_SENDTO);
+        // selectorIntent.setData(Uri.parse("mailto:"));
+        // Intent emailIntent = new Intent(Intent.ACTION_SEND);
+        // emailIntent.setData(Uri.parse("mailto:"));
+        // emailIntent.putExtra(Intent.EXTRA_EMAIL, new String[]{"cbnusinabro@gmail.com"});
+        // emailIntent.putExtra(Intent.EXTRA_SUBJECT, "[문의]: ");
+        // emailIntent.setSelector(selectorIntent);
+        // startActivity(Intent.createChooser(emailIntent, "Send email..."));
 
         return view;
     }
