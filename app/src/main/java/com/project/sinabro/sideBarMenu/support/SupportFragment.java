14a38e659544581ea036a9e73eaaf52c898870bd
developersung13 <developersung13@gmail.com>
2023-02-27 10:34:39 +0900

    [feat]: '문의' fragment 제어 자바 파일 추가 (#58)

---
+++ app/src/main/java/com/project/sinabro/sideBarMenu/support/SupportFragment.java
@@ -0,0 +1,86 @@
+package com.project.sinabro.sideBarMenu.support;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.project.sinabro.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link SupportFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class SupportFragment extends Fragment {
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    public SupportFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment SupportFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static SupportFragment newInstance(String param1, String param2) {
+        SupportFragment fragment = new SupportFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_empty, container, false);
+        /** 메일 문의 선택 방식(1) */
+        Intent intent = new Intent(Intent.ACTION_SENDTO);
+        intent.setData(Uri.parse("mailto:")); // only email apps should handle this
+        intent.putExtra(Intent.EXTRA_EMAIL, new String[]{"cbnusinabro@gmail.com"});
+        intent.putExtra(Intent.EXTRA_SUBJECT, "[문의]: ");
+        startActivity(intent);
+
+        /** 메일 문의 선택 방식(2)  */
+        // Intent selectorIntent = new Intent(Intent.ACTION_SENDTO);
+        // selectorIntent.setData(Uri.parse("mailto:"));
+        // Intent emailIntent = new Intent(Intent.ACTION_SEND);
+        // emailIntent.setData(Uri.parse("mailto:"));
+        // emailIntent.putExtra(Intent.EXTRA_EMAIL, new String[]{"cbnusinabro@gmail.com"});
+        // emailIntent.putExtra(Intent.EXTRA_SUBJECT, "[문의]: ");
+        // emailIntent.setSelector(selectorIntent);
+        // startActivity(Intent.createChooser(emailIntent, "Send email..."));
+
+        return view;
+    }
+}
