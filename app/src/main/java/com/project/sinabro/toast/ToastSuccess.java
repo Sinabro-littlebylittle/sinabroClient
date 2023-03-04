4c36375c668b626be468a39fa44d94ce98d31577
developersung13 <developersung13@gmail.com>
2023-03-04 20:14:03 +0900

    [fix]: '성공' 관련 toast message 자바 파일명 변경으로 인한 신규 파일 추가 (#62)

---
+++ app/src/main/java/com/project/sinabro/toast/ToastSuccess.java
@@ -0,0 +1,23 @@
+package com.project.sinabro.toast;
+
+import android.app.Activity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.project.sinabro.R;
+
+public class ToastSuccess {
+    public ToastSuccess(String message, Activity activity) {
+        Toast toast = new Toast(activity);
+        View view = LayoutInflater.from(activity)
+                .inflate(R.layout.toast_success, null);
+
+        TextView tvMessage = view.findViewById(R.id.toast_modify_success_tv);
+        tvMessage.setText(message);
+
+        toast.setView(view);
+        toast.show();
+    }
+}
