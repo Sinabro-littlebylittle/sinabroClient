5666e8f9bf1b2073176989e6b8ae073ba952693d
YEOLAM <developersung13@gmail.com>
2023-05-09 22:37:39 +0900

    [feat]: CameraX 액티비티 디자인 수정 및 인원수 Detection 기능 활성화 (#80) (#81)
    * [feat]: UI 요소 확대 애니메이션 XML 파일 추가 (#80)
    
    * [feat]: 인원수 검출 결과 내용이 담긴 Dialog XML 파일 추가 (#80)
    
    * [feat]: 객체 검출 화면 액티비티 디자인 수정 (#80)
    
    * [feat]: 스캔 후 인원수 결과를 통한 Bottom Sheet 내 요소 데이터 갱신 로직 추가 (#80)
    
    * [feat]: 스캔 후 확인된 인원수 데이터 MainActivity로 전달해 주는 로직 추가 (#80)
    
    * [feat]: 신규 장소에 대한 스캔 버튼 클릭 시 처리 로직 추가 (#80)

--- app/src/main/java/com/project/sinabro/AbstractCameraXActivity.java
+++ app/src/main/java/com/project/sinabro/AbstractCameraXActivity.java
@@ -2,15 +2,23 @@ package com.project.sinabro;
 
 import android.Manifest;
 import android.app.AlertDialog;
+import android.app.Dialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.os.SystemClock;
+import android.util.Log;
 import android.util.Size;
 import android.view.TextureView;
 import android.view.View;
+import android.view.Window;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.Nullable;
@@ -24,12 +32,18 @@ import androidx.camera.core.Preview;
 import androidx.camera.core.PreviewConfig;
 import androidx.core.app.ActivityCompat;
 
+import com.project.sinabro.bottomSheet.place.AddLocationInfoActivity;
+import com.project.sinabro.bottomSheet.place.PlaceListActivity;
+import com.project.sinabro.toast.ToastSuccess;
+
 public abstract class AbstractCameraXActivity<R> extends BaseModuleActivity {
     private static final int REQUEST_CODE_CAMERA_PERMISSION = 200;
     private static final String[] PERMISSIONS = {Manifest.permission.CAMERA};
 
     private long mLastAnalysisResultTime;
     private Button scanbtn;
+
+    private Dialog peopleCountDetectionResult_dialog;
 
 
     protected abstract int getContentViewLayoutId();
@@ -53,33 +67,52 @@ public abstract class AbstractCameraXActivity<R> extends BaseModuleActivity {
             setupCameraX();
         }
 
-        scanbtn=findViewById(com.project.sinabro.R.id.scanbutton);
+        peopleCountDetectionResult_dialog = new Dialog(AbstractCameraXActivity.this);  // Dialog 초기화
+        peopleCountDetectionResult_dialog.requestWindowFeature(Window.FEATURE_NO_TITLE); // 타이틀 제거
+        peopleCountDetectionResult_dialog.setContentView(com.project.sinabro.R.layout.dialog_people_count_detection_result); // xml 레이아웃 파일과 연결
+        // dialog 창의 root 레이아웃을 투명하게 조절 모서리(코너)를 둥글게 보이게 하기 위해
+        peopleCountDetectionResult_dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+
+        Animation animation = AnimationUtils.loadAnimation(this, com.project.sinabro.R.anim.ripple_animation);
+        scanbtn = findViewById(com.project.sinabro.R.id.scanbutton);
+        scanbtn.startAnimation(animation);
         scanbtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //카메라 화면에서 완료버튼 클릭시
-                int temp = ResultView.countP();
-                AlertDialog.Builder builder=new AlertDialog.Builder(AbstractCameraXActivity.this);
-                builder.setTitle("Detection Result").setMessage(String.valueOf(temp)+" is detected");
-                builder.setPositiveButton("확인", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        Toast.makeText(AbstractCameraXActivity.this, "확인", Toast.LENGTH_SHORT).show();
-//                            AbstractCameraXActivity.super.onBackPressed();
-//                            AbstractCameraXActivity.this.finish();
-                        finish();
-                        Intent intent = new Intent(AbstractCameraXActivity.this, MainActivity.class);
-                        startActivity(intent);
-                        ResultView.sum=0;
-//                            String headcount=String.valueOf(temp);
+                //카메라 화면에서 완료버튼 클릭 시
+                showDialog_people_count_detection_result();
+            }
+        });
+    }
 
-                    }
-                });
+    // (dialog_place_remove) 다이얼로그를 디자인하는 함수
+    public void showDialog_people_count_detection_result() {
+        int peopleCnt = ResultView.countP();
+        ResultView.sum=0;
+        TextView peopleCountDetectionResult_tv = peopleCountDetectionResult_dialog.findViewById(com.project.sinabro.R.id.peopleCountDetectionResult_tv);
+        peopleCountDetectionResult_tv.setText((String.valueOf(peopleCnt) + "명의 인원이 확인되었습니다"));
+        peopleCountDetectionResult_dialog.show(); // 다이얼로그 띄우기
+        // 다이얼로그 창이 나타나면서 외부 액티비티가 어두워지는데, 그 정도를 조절함
+        peopleCountDetectionResult_dialog.getWindow().setDimAmount(0.35f);
 
+        // "취소" 버튼
+        peopleCountDetectionResult_dialog.findViewById(com.project.sinabro.R.id.noBtn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                peopleCountDetectionResult_dialog.dismiss(); // 다이얼로그 닫기
+            }
+        });
 
-//                Toast.makeText(getBaseContext(), "test scanbtn.", Toast.LENGTH_LONG).show();
-                AlertDialog alertDialog= builder.create();
-                alertDialog.show();
+        // "확인" 버튼
+        peopleCountDetectionResult_dialog.findViewById(com.project.sinabro.R.id.yesBtn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                peopleCountDetectionResult_dialog.dismiss(); // 다이얼로그 닫기
+                finish();
+                final Intent intent = new Intent(AbstractCameraXActivity.this, MainActivity.class);
+                intent.putExtra("isDetected", true);
+                intent.putExtra("peopleCountDetectionResult", peopleCnt);
+                startActivity(intent);
             }
         });
     }
