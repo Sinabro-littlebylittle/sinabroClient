c98d45e0bd2998a240800f77bdefcfe6f24256a5
developersung13 <developersung13@gmail.com>
2023-06-03 14:16:20 +0900

    [feat]: 'Places' collection 관련 APIs 인터페이스 정의 및 선언 (#82)

--- app/src/main/java/com/project/sinabro/retrofit/PlacesAPI.java
+++ app/src/main/java/com/project/sinabro/retrofit/PlacesAPI.java
@@ -1,18 +1,27 @@
 package com.project.sinabro.retrofit;
 
-import com.project.sinabro.model.Places;
+import com.project.sinabro.models.Place;
 
 import java.util.List;
 
 import retrofit2.Call;
 import retrofit2.http.Body;
+import retrofit2.http.DELETE;
 import retrofit2.http.GET;
+import retrofit2.http.PATCH;
 import retrofit2.http.POST;
+import retrofit2.http.Path;
 
 public interface PlacesAPI {
-    @GET("/api/places")
-    Call<List<Places>> getAllEmployees();
+    @GET("/places")
+    Call<List<Place>> getPlaces();
 
-    @POST("/api/places/save")
-    Call<Places> save(@Body Places places);
+    @POST("/places")
+    Call<Place> addPlaceInformation(@Body Place place);
+
+    @PATCH("/places/{id}")
+    Call<Place> updatePlace(@Path("id") String id, @Body Place place);
+
+    @DELETE("/places/{id}")
+    Call<Integer> deletePlace(@Path("id") String id);
 }
