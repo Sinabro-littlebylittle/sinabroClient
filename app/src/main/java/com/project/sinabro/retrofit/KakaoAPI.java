6e7ccce7385992f6e4c8892805540f6e354fd4f9
developersung13 <developersung13@gmail.com>
2023-06-03 14:40:51 +0900

    [feat]: 위치 기반 도로명 주소 불러오기 API에 대한 interface 정의 및 선언 (#82)

---
+++ app/src/main/java/com/project/sinabro/retrofit/KakaoAPI.java
@@ -0,0 +1,12 @@
+package com.project.sinabro.retrofit;
+
+import com.project.sinabro.models.CoordinateToAddress;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
+public interface KakaoAPI {
+    @GET("v2/local/geo/coord2address.json")
+    Call<CoordinateToAddress> getAddress(@Query("input_coord") String input_coord, @Query("x") double x, @Query("y") double y);
+}
