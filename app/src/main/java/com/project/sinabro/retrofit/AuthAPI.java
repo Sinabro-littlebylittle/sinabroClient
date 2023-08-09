518181269a464268df0111e7157f080d5396c2b9
developersung13 <developersung13@gmail.com>
2023-08-09 23:46:16 +0900

    [feat]: 'auth' router에 대한  APIs 전용 interface 파일 추가 (#91)

---
+++ app/src/main/java/com/project/sinabro/retrofit/AuthAPI.java
@@ -0,0 +1,22 @@
+package com.project.sinabro.retrofit;
+
+import com.project.sinabro.models.requests.LoginRequest;
+import com.project.sinabro.models.UserInfo;
+
+import okhttp3.ResponseBody;
+import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.GET;
+import retrofit2.http.POST;
+import retrofit2.http.Query;
+
+public interface AuthAPI {
+    @GET("/api/auth/public/search")
+    Call<ResponseBody> checkExistEmail(@Query("email") String email);
+
+    @POST("/api/auth/public/login")
+    Call<ResponseBody> login(@Body LoginRequest loginRequest);
+
+    @POST("/api/auth/public/signup")
+    Call<UserInfo> signUp(@Body UserInfo userinfo);
+}
