907400ba55399aff6aebb3b81e82b90cdcfb6492
developersung13 <developersung13@gmail.com>
2023-08-10 14:48:28 +0900

    [feat]: JWT 인증 방식 도입으로 인한 authInterceptor 추가 (#96)

--- app/src/main/java/com/project/sinabro/retrofit/RetrofitService.java
+++ app/src/main/java/com/project/sinabro/retrofit/RetrofitService.java
@@ -1,20 +1,30 @@
 package com.project.sinabro.retrofit;
 
 import com.google.gson.Gson;
+import com.project.sinabro.utils.AuthInterceptor;
+import com.project.sinabro.utils.TokenManager;
 
+import okhttp3.OkHttpClient;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class RetrofitService {
     private Retrofit retrofit;
+    private AuthInterceptor authInterceptor;
 
-    public RetrofitService() {
+    public RetrofitService(TokenManager tokenManager) {
+        authInterceptor = new AuthInterceptor(tokenManager);
         initializeRetrofit();
     }
 
     private void initializeRetrofit() {
+        OkHttpClient client = new OkHttpClient.Builder()
+                .addInterceptor(authInterceptor)
+                .build();
+
         retrofit = new Retrofit.Builder()
-                .baseUrl("http://sinabro-developersung13.koyeb.app")
+                .baseUrl("http://10.0.2.2:5050")
+                .client(client)
                 .addConverterFactory(GsonConverterFactory.create(new Gson()))
                 .build();
     }
