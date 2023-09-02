98036a74aa692c109e279902a544b61554e8a6aa
developersung13 <developersung13@gmail.com>
2023-09-02 23:43:41 +0900

    [fix]: headcountAPI 파일명 및 경로 변경 (#134)

--- app/src/main/java/com/project/sinabro/retrofit/interfaceAPIs/HeadcountsAPI.java
+++ app/src/main/java/com/project/sinabro/retrofit/interfaceAPIs/HeadcountsAPI.java
@@ -1,4 +1,4 @@
-package com.project.sinabro.retrofit;
+package com.project.sinabro.retrofit.interfaceAPIs;
 
 import com.project.sinabro.models.Place;
 import com.project.sinabro.models.Headcount;
@@ -11,12 +11,12 @@ import retrofit2.http.GET;
 import retrofit2.http.POST;
 import retrofit2.http.Path;
 
-public interface headcountsAPI {
+public interface HeadcountsAPI {
     @GET("/api/headcounts/public/placeInformations")
     Call<List<Headcount>> getPlaceInformations();
 
-    @GET("/api/headcounts/public/{id}/placeInformations")
-    Call<List<Headcount>> getPlaceInformationsById(@Path("id") String id);
+    @GET("/api/headcounts/public/placeInformations/markers/{markerId}")
+    Call<List<Headcount>> getPlaceInformationsById(@Path("markerId") String markerId);
 
     @POST("/api/headcounts/private")
     Call<Place> addPeopleNumber(@Body Headcount peopleNumber);
