1e2a8e353404d1dae7a983cd95ff5071bd2a8fac
developersung13 <developersung13@gmail.com>
2023-09-06 09:40:19 +0900

    [feat]: '임시 비밀번호 생성' API 사용 코드 추가 (#148)

--- app/src/main/java/com/project/sinabro/sideBarMenu/authentication/ResetPasswordActivity.java
+++ app/src/main/java/com/project/sinabro/sideBarMenu/authentication/ResetPasswordActivity.java
@@ -12,7 +12,23 @@ import android.view.Window;
 
 import com.project.sinabro.R;
 import com.project.sinabro.databinding.ActivityResetPasswordBinding;
+import com.project.sinabro.models.UserInfo;
+import com.project.sinabro.retrofit.RetrofitService;
+import com.project.sinabro.retrofit.interfaceAPIs.UserAPI;
+import com.project.sinabro.sideBarMenu.settings.MyPageActivity;
 import com.project.sinabro.textWatcher.EmailWatcher;
+import com.project.sinabro.toast.ToastWarning;
+import com.project.sinabro.utils.TokenManager;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+
+import okhttp3.ResponseBody;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class ResetPasswordActivity extends AppCompatActivity {
 
@@ -20,11 +36,19 @@ public class ResetPasswordActivity extends AppCompatActivity {
 
     private Dialog resetPasswordSuccess_dialog;
 
+    private TokenManager tokenManager;
+    private RetrofitService retrofitService;
+    UserAPI userAPI;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivityResetPasswordBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+
+        tokenManager = TokenManager.getInstance(this);
+        retrofitService = new RetrofitService(tokenManager);
+        userAPI = retrofitService.getRetrofit().create(UserAPI.class);
 
         /** "비밀번호 초기화 완료 안내" 다이얼로그 변수 초기화 및 설정 */
         resetPasswordSuccess_dialog = new Dialog(ResetPasswordActivity.this);  // Dialog 초기화
@@ -37,7 +61,6 @@ public class ResetPasswordActivity extends AppCompatActivity {
         binding.backIBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                onBackPressed(); // 뒤로가기 기능 수행
                 finish(); // 현재 액티비티 종료
             }
         });
@@ -58,13 +81,36 @@ public class ResetPasswordActivity extends AppCompatActivity {
                     return;
                 }
 
-                /* 이곳에 API 호출 코드가 추가되어야 합니다. */
+                binding.resetPasswordFailedTv.setVisibility(View.GONE);
 
-                // 입력된 이메일이 정상적으로 DB에서 조회되었을 때 다이얼로그 띄우기
-//                 showDialog_reset_password_success();
+                UserInfo userInfo = new UserInfo();
+                userInfo.setEmail(binding.emailEditText.getText().toString());
 
-                // 입력된 이메일이 DB에 존재하지 않을 경우 실패 메시지 보이게 하기
-                binding.resetPasswordFailedTv.setVisibility(View.VISIBLE);
+                Call<UserInfo> call_userAPI_generateTempPassword = userAPI.generateTempPassword(userInfo);
+                call_userAPI_generateTempPassword.enqueue(new Callback<UserInfo>() {
+                    @Override
+                    public void onResponse(Call<UserInfo> call, Response<UserInfo> response) {
+                        if (response.isSuccessful()) {
+                            // 입력된 이메일이 정상적으로 DB에서 조회되었을 때 다이얼로그 띄우기
+                            showDialog_reset_password_success();
+                        } else {
+                            switch (response.code()) {
+                                case 400:
+                                case 404:
+                                    binding.resetPasswordFailedTv.setVisibility(View.VISIBLE);
+                                    break;
+                                default:
+                                    new ToastWarning(getResources().getString(R.string.toast_none_status_code), ResetPasswordActivity.this);
+                            }
+                        }
+                    }
+
+                    @Override
+                    public void onFailure(Call<UserInfo> call, Throwable t) {
+                        // 서버 코드 및 네트워크 오류 등의 이유로 요청 실패
+                        new ToastWarning(getResources().getString(R.string.toast_server_error), ResetPasswordActivity.this);
+                    }
+                });
             }
         });
     }
