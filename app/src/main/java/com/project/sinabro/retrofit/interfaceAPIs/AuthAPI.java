964bfe26fcf69d4e68fd4ae0403cfd66bb853944
developersung13 <developersung13@gmail.com>
2023-09-12 20:59:37 +0900

    [feat]: auth 인터페읏 내에 '회원 탈퇴 및 사유 기록' API 엔드포인트 메소드 추가 (#159)

--- app/src/main/java/com/project/sinabro/retrofit/interfaceAPIs/AuthAPI.java
+++ app/src/main/java/com/project/sinabro/retrofit/interfaceAPIs/AuthAPI.java
@@ -1,12 +1,17 @@
 package com.project.sinabro.retrofit.interfaceAPIs;
 
+import com.project.sinabro.models.UserWithdrawalReason;
 import com.project.sinabro.models.requests.LoginRequest;
 import com.project.sinabro.models.UserInfo;
+
+import java.util.List;
 
 import okhttp3.ResponseBody;
 import retrofit2.Call;
 import retrofit2.http.Body;
+import retrofit2.http.DELETE;
 import retrofit2.http.GET;
+import retrofit2.http.HTTP;
 import retrofit2.http.POST;
 import retrofit2.http.Query;
 
@@ -19,4 +24,7 @@ public interface AuthAPI {
 
     @POST("/api/auth/public/sign-up")
     Call<UserInfo> signUp(@Body UserInfo userinfo);
+
+    @HTTP(method = "DELETE", path = "/api/auth/private/delete-account", hasBody = true)
+    Call<ResponseBody> deleteAccount(@Body UserWithdrawalReason userWithdrawalReason);
 }
