0225fd015e56401cd2ecbe04170672058b076e53
developersung13 <developersung13@gmail.com>
2023-09-12 21:07:17 +0900

    [feat]: 'userWithdrawalReason' collection에 대한 스키마 모델 POJO 클래스 파일 추가 (#159)

---
+++ app/src/main/java/com/project/sinabro/models/UserWithdrawalReason.java
@@ -0,0 +1,31 @@
+package com.project.sinabro.models;
+
+public class UserWithdrawalReason {
+    private String withdrawalReason;
+    private String feedback;
+    private String createdTime;
+
+    public String getWithdrawalReason() {
+        return withdrawalReason;
+    }
+
+    public void setWithdrawalReason(String withdrawalReason) {
+        this.withdrawalReason = withdrawalReason;
+    }
+
+    public String getFeedback() {
+        return feedback;
+    }
+
+    public void setFeedback(String feedback) {
+        this.feedback = feedback;
+    }
+
+    public String getCreatedTime() {
+        return createdTime;
+    }
+
+    public void setCreatedTime(String createdTime) {
+        this.createdTime = createdTime;
+    }
+}
